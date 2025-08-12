13f9d4591a19155b855fca5168db8fa2fd524cea
barrymurphybhr <barry.murphy@brighthr.com>
2025-08-12 16:57:48 +0100

    added brightsafe feature page tests

---
+++ tests/brightsafeFeature.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+
+const featurePages = [
+  "health-and-safety-software",
+  "accident-near-miss-reporting",
+  "coshh-risk-assessment",
+  "brightsafe-on-the-go",
+  "health-and-safety-advice-line",
+  "health-and-safety-insurance",
+  "health-and-safety-elearning",
+  "brightlearn-health-and-safety",
+];
+
+featurePages.forEach((feature) => {
+  test(`brightsafe feature page visual test | @brightsafeFeature | ${feature}`, async ({
+    page,
+  }) => {
+    await page.goto(`/brightsafe/${feature}`);
+    await page.getByRole("button", { name: "Accept all" }).click();
+    await expect(page).toHaveScreenshot(`${feature}.png`, { fullPage: true });
+  });
+});
