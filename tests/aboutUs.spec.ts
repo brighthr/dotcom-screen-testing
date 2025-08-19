4448fc6e953c203c88029ee0fbf9e03a1ca66cb8
barrymurphybhr <barry.murphy@brighthr.com>
2025-08-19 13:41:04 +0100

    added who we serve

--- tests/aboutUs.spec.ts
+++ tests/aboutUs.spec.ts
@@ -10,10 +10,10 @@ const aboutUsPages = [
 ];
 
 aboutUsPages.forEach((aboutUsPage) => {
-  test(`about us page visual test | @aboutUs | ${aboutUsPage}`, async ({
+  test(`about us page visual test | @aboutUsPage | ${aboutUsPage}`, async ({
     page,
   }) => {
-    await page.goto(`${aboutUsPage}`);
+    await page.goto(`/${aboutUsPage}`);
     await page.getByRole("button", { name: "Accept all" }).click();
     await expect(page).toHaveScreenshot(`${aboutUsPage}.png`, { fullPage: true });
   });
